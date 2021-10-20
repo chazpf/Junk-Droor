076ffc5ef5dbf546f8b6e38e8878292606c95de3
Charles (Chaz) Formichella <74632592+chazpf@users.noreply.github.com>
2021-10-20 10:38:34 -0400

    Initial commit - server setup and database connection setup

---
+++ server.js
@@ -0,0 +1,31 @@
+// Dependencies
+const express = require('express');
+const methodOverride  = require('method-override');
+const mongoose = require ('mongoose');
+const app = express();
+const db = mongoose.connection;
+require('dotenv').config()
+
+// Configuration
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// Database Connection
+mongoose.connect(MONGODB_URI);
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+//Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+
+// Routes
+app.get('/' , (req, res) => {
+  res.send('Hello World!');
+});
+
+//Listener
+app.listen(PORT, () => console.log( 'Listening on port:', PORT));
