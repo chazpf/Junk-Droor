640283ca67cd1f20eecbf1260d58804289ded96b
Charles (Chaz) Formichella <74632592+chazpf@users.noreply.github.com>
2021-10-21 19:59:06 -0400

    drawers model created

---
+++ models/drawers.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const drawerSchema = Schema({
+  name: { type: String, required: true },
+  fullness: { type: String, required: true },
+  owner: { type: String, required: true },
+  items: []
+})
+
+const Drawer = mongoose.model('Drawer', drawerSchema);
+
+module.exports = Drawer;
